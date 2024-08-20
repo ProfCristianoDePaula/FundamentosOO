2e125a9cab82c6a7dc04d05491ae61d2a4694b54
Cristiano de Paula <prof.cristianodepaula@gmail.com>
2024-08-20 10:43:41 -0300

    Adicionar arquivos de projeto.

---
+++ Fundamentos/Carro.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentos
+{
+    public class Carro
+    {
+        // Atributos
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Velocidade { get; set; }
+        // Métodos
+        public void Acelerar()
+        {
+            this.Velocidade += 5;
+            Console.WriteLine("A Velocidade Atual é " + this.Velocidade + " km/h");
+        }
+        public void Desacelerar()
+        {
+            this.Velocidade -= 5;
+            Console.WriteLine("A Velocidade Atual é " + this.Velocidade + " km/h");
+        }
+    }
+}
