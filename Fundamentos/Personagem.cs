703211f59d65d34710b5b10c3ca36cc93dbdadc6
Cristiano de Paula <prof.cristianodepaula@gmail.com>
2024-08-21 08:17:55 -0300

    Finalizada a atividade do Game

--- Fundamentos/Personagem.cs
+++ Fundamentos/Personagem.cs
@@ -18,7 +18,14 @@ namespace Fundamentos
 
         // Metodos
         public virtual void apresentarSe() { }
-        public virtual void evoluir() { }
+        public virtual void evoluir()
+        {
+            this.Nivel += 2;
+            this.Forca *= 2;
+            this.Inteligencia *= 2;
+            this.Agilidade *= 2;
+            Console.WriteLine("O Jogador " + this.Nome + " evoluiu para o nível " + this.Nivel + ". Seus novos valores são: \nAgilidade: " + this.Agilidade + "\nInteligência: " + this.Inteligencia + "\nForça: " + this.Forca);
+        }
         public virtual int atacar()
         {
             int pontosAtaque = 0;
@@ -63,12 +70,14 @@ namespace Fundamentos
                 {
                     Console.WriteLine("O Personagem " + p.Nome + " Venceu!");
                     vencedor = true;
+                    p.evoluir();
                 }
 
                 if (p.Vida <= 0)
                 {
                     Console.WriteLine("O Personagem " + this.Nome + " Venceu!");
                     vencedor = true;
+                    this.evoluir();
                 }
 
             }
